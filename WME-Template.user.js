3f4eecfe047b9189112c9e8270a6640954488f3d
Anton Shevchuk <antonshevchuk@gmail.com>
2023-01-06 12:11:00 +0100

    Updated Boostrap version

--- WME-Template.user.js
+++ WME-Template.user.js
@@ -12,7 +12,7 @@
 // @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://anton.shevchuk.name&size=64
 // @grant        none
 // @require      https://greasyfork.org/scripts/389765-common-utils/code/CommonUtils.js?version=1090053
-// @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1128320
+// @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1135567
 // @require      https://greasyfork.org/scripts/452563-wme/code/WME.js?version=1101598
 // @require      https://greasyfork.org/scripts/450221-wme-base/code/WME-Base.js?version=1129908
 // @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1134661
